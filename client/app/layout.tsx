6bd8e58697706b921882bde57ea2e225025aa733
Aryan Singh <aryan28november@gmail.com>
2025-04-05 09:53:44 +0530

    analytics update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { PostHogProvider } from "../components/PostHogProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     "Node.js",
     "Next.js",
     "Backend Tools",
-    "API Mocking",
+    "API Mocking"
   ],
   authors: [{ name: "Your Name", url: "https://yourdomain.com" }],
   creator: "Your Name or Brand",
@@ -41,36 +41,34 @@ export const metadata: Metadata = {
         url: "https://yourdomain.com/og-image.png",
         width: 1200,
         height: 630,
-        alt: "Mongoose Schema Parser",
-      },
+        alt: "Mongoose Schema Parser"
+      }
     ],
     locale: "en_US",
-    type: "website",
+    type: "website"
   },
   twitter: {
     card: "summary_large_image",
     title: "Mongoose Schema Parser & Dummy Data Generator",
-    description:
-      "Auto-generate mock data from Mongoose schemas with one click.",
+    description: "Auto-generate mock data from Mongoose schemas with one click.",
     creator: "@yourhandle",
-    images: ["https://yourdomain.com/og-image.png"],
+    images: ["https://yourdomain.com/og-image.png"]
   },
-  themeColor: "#ff6b00",
+  themeColor: "#ff6b00"
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-black dark:text-white`}
-      >
-        <main>
-          <Analytics />
-          {children}</main>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-black dark:text-white`}>
+        <PostHogProvider>
+          <main>
+          
+            {children}
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
